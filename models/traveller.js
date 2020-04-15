49faf555c9c27066ec6acad366ed381cf40f5158
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-04-15 16:48:38 +0100

    can find startLocations

---
+++ models/traveller.js
@@ -0,0 +1,32 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  let result = this.journeys.map(journey => journey.startLocation);
+  return result;
+
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
